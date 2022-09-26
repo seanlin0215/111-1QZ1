cba6976e3a6a2d316a9170afa7f05c4a743db5ea
seanlin0215 <seanlin0215@gmail.com>
2022-09-26 14:32:39 +0800

    4356

---
+++ 111-1QZ1/Bomb.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _111_1QZ1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int[] ia_Mlndex = new int[10] { 0, 7, 13, 28, 44, 62, 74, 75, 87, 90 };
+            char[,] ia_Map = new char[10, 10];
+            for (int i_Row = 0; i < 10; i_Row++)
+            {
+                for (int i_Col = 0; i_Col < 10; i_Col++)
+                {
+                    ia_Map[i_Row, i_Col] = "0";
+                }
+            }
+
+            for (int i_Ct = 0; i_Ct < 10; i_Ct++)
+            {
+                int i_Row = ia_Mlndex / 10;
+                int i_Col = ia_Mlndex % 10;
+                ia_Map[i_Row i_Col] = "*";
+            }
+
+            /*尋訪2D array */
+            for (int i_Row = 0; i_Row < 10; i_Row++)
+            {
+                for (int i_Col = 0; i_Col < 10; i_Col++)
+                {
+                    if (ia_Map[i_Row, i_Col] == '') {
+                        Response.Write("&nbsp");
+                    }
+                    else {
+
+                    }
+                    Response.Write(ia_Map[i_Row, i_Col]);
+                }
+                Response.Write("<br />");
+            }
+        }
+    }
+}
